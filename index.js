ab92c5dbff6d905519810a8b8f185eba16bfcff1
Stiflerzak <hilarymwendia97@gmail.com>
2022-11-24 23:00:44 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+   let num1= 2000
+   let num2=100
+let result=num1/num2 
+    return result;
+  }
+  divide();
+
+  const square = x => x * x;
+  square(3);
+
+  const add = (x,y) => x + y;
+  add(4,5)
